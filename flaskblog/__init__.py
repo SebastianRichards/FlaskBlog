055f0e9f2d63f3c705d76d0cb96494d0093d95c0
Sebastian Richards <sebastianrichards@me.com>
2025-10-25 18:24:11 +0100

    db variables

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b83887e39f9645a578b9697b9fd62cd8'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/sebastianrichards_db'
+
+# Use environment variable for database URL, fallback to local for development
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://localhost/sebastianrichards_db')
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['APPLICATION_ROOT'] = '/blog'
 
