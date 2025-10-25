4dc7a1039682fee13b4a53cf7d8f638ec060fb7e
Sebastian Richards <sebastianrichards@me.com>
2025-10-25 18:33:11 +0100

    url fixes

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -15,6 +15,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['APPLICATION_ROOT'] = '/blog'
 
+# Add middleware to set SCRIPT_NAME for proper URL generation
+class PrefixMiddleware(object):
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/blog')
+
 
 
 db = SQLAlchemy(app)
